0b339fa8bb03a5bafd8cf6b449daf422078c5508
Prokhorik <makctop228228@gmail.com>
2026-04-17 08:59:42 +0300

    Lab10: add Dockerfile, tests and solution

---
+++ IsLabApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace IsLabApp.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void PassingTest() => Assert.Equal(4, Add(2, 2));
+    [Fact]
+    public void AnotherPassingTest() => Assert.Equal(0, Add(0, 0));
+    static int Add(int x, int y) => x + y;
+}
